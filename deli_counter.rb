0852a8322ae8fbf5ff1d79a17af2fd3fa3e219e8
shadowfox3735 <normanpan180@gmail.com>
2018-11-07 17:26:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,9 @@ def line(deli)
     puts "The line is currently empty."
   else 
     x = 0
-    "The line is currently: "
+    line = "The line is currently: "
     while x < deli.length
-    "#{x+1}. #{deli[x]}"
+    line << "#{x+1}. #{deli[x]}"
     x += 1
   end
 end
